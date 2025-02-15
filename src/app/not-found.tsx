0266f2b22d87c2e0a926020070671aa00f151ed3
Eman Gurung <egurung1117@gmail.com>
2025-02-15 02:35:38 -0500

    private folders

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <h2>Page Not Found</h2>
+      <p>Could not find request resource</p>
+    </div>
+  )
+}
